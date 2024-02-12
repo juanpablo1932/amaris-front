9340a5ca1d510b977354c9b7021cab3ecc30f9bd
Juan Pablo <juandiazecheverry@gmail.com>
2024-02-11 23:27:26 -0500

    add bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    global:'window'
+  },
+  envDir: 'environments',
+  optimizeDeps: {
+    disabled: false,
+  },
+  publicDir:'public',
+  server: {
+    host: 'localhost',
+    port: 3001,
+    strictPort: true
+  }
 })
